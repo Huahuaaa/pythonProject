f601ed47dde820236456cb96737b3c5496888d7a
Administrator <97603875@qq.com>
2022-05-18 16:06:23 +0800

    测试

---
+++ pythonProject/4.py
@@ -0,0 +1,11 @@
+def a(a, b):
+    for i in range(a):
+        for j in range(b):
+            if i == 0 or i == a - 1 or j == 0 or j == b - 1:
+                print('*', end='')
+            else:
+                print('-', end='')
+        print()
+
+
+a(6, 6)
---
+++ pythonProject/函数.py
@@ -0,0 +1,9 @@
+# 注释
+
+def c():
+    a = int(input('请输入a的值：'))
+    b = int(input('请输入b的值：'))
+    return a * b
+
+
+print(c())
